c7622c76dfd6da16deeef674d379e74ab1b4a25b
kav3321 <kavp231@gmail.com>
2026-06-28 21:54:21 +1000

    Update redirect URL in intro.js

--- intro/intro.js
+++ intro/intro.js
@@ -2,5 +2,5 @@
 // zoom-in starts at 0.5s delay, runs 3.5s = finishes at 4s
 // Small buffer added = redirect at 4.2s
 setTimeout(function() {
-  window.location.href = 'home.html';
+  window.location.href = '/Netflix/home.html';
 }, 4200);
